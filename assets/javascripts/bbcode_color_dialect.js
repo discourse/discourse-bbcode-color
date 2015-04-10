e83d6dba52d157a354a2a87dc56b82b2012dd053
jqnatividad <joel.natividad@ontodia.com>
2015-04-10 01:31:05 -0400

    add background-color support

--- assets/javascripts/bbcode_color_dialect.js
+++ assets/javascripts/bbcode_color_dialect.js
@@ -6,6 +6,15 @@
     return text;
   }
 
+  function replaceFontBgColor (text) {
+    while (text != (text = text.replace(/\[bgcolor=([^\]]+)\]((?:(?!\[bgcolor=[^\]]+\]|\[\/bgcolor\])[\S\s])*)\[\/bgcolor\]/ig, function (match, p1, p2, offset, string) {
+      return "<span style='background-color: " + p1 + "'>" + p2 + "</span>";
+    })));
+    return text;
+  }
+
   Discourse.Dialect.addPreProcessor(replaceFontColor);
+  Discourse.Dialect.addPreProcessor(replaceFontBgColor);
   Discourse.Markdown.whiteListTag('font', 'color');
+  Discourse.Markdown.whiteListTag('span', 'style', /^background-color:.*$/i);
 })();
