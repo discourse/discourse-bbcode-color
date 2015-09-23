14a02fafbae84e5826c1a771d6bc49625b44fb1b
Dan Dascalescu <ddascalescu+github@gmail.com>
2015-09-23 03:51:52 -0700

    Add URL so the plugin is clickable in Dashboard

--- plugin.rb
+++ plugin.rb
@@ -2,5 +2,6 @@
 # about: A Discourse plugin to support BBCode color tags.
 # version: 0.1
 # authors: Neil Lalonde
+# url: https://github.com/discourse/discourse-bbcode-color/
 
 register_asset "javascripts/bbcode_color_dialect.js", :server_side
