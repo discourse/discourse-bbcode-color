e3edc2ef23b7104f1e4d145f774dd2b8f209576f
Régis Hanol <regis@hanol.fr>
2016-07-12 16:31:37 +0200

    keep old code around for compatibility

---
+++ assets/javascripts/bbcode_color_dialect.js
@@ -0,0 +1,20 @@
+(function() {
+  function replaceFontColor (text) {
+    while (text !== (text = text.replace(/\[color=([^\]]+)\]((?:(?!\[color=[^\]]+\]|\[\/color\])[\S\s])*)\[\/color\]/ig, function (match, p1, p2) {
+      return "<font color='" + p1 + "'>" + p2 + "</font>";
+    })));
+    return text;
+  }
+
+  function replaceFontBgColor (text) {
+    while (text !== (text = text.replace(/\[bgcolor=([^\]]+)\]((?:(?!\[bgcolor=[^\]]+\]|\[\/bgcolor\])[\S\s])*)\[\/bgcolor\]/ig, function (match, p1, p2) {
+      return "<span style='background-color: " + p1 + "'>" + p2 + "</span>";
+    })));
+    return text;
+  }
+
+  Discourse.Dialect.addPreProcessor(replaceFontColor);
+  Discourse.Dialect.addPreProcessor(replaceFontBgColor);
+  Discourse.Markdown.whiteListTag('font', 'color');
+  Discourse.Markdown.whiteListTag('span', 'style', /^background-color:.*$/i);
+})();
