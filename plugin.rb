3f6a0e1663c2aa04f2c4d397fa9b1790c08911ce
Guo Xiang Tan <tgx_world@hotmail.com>
2017-07-06 11:30:14 +0900

    Remove unused file.

--- plugin.rb
+++ plugin.rb
@@ -3,5 +3,3 @@
 # version: 0.2
 # authors: Neil Lalonde, Régis Hanol
 # url: https://github.com/discourse/discourse-bbcode-color/
-
-register_asset "javascripts/bbcode_color_dialect.js", :server_side
