288872d7b8c3f994f44fd004418643b71bd16cac
Jarek Radosz <jradosz@gmail.com>
2022-06-11 23:40:13 +0200

    DEV: Don't use the deprecated `font` tag (#38)
    Use `span` with inline style instead. See also: https://github.com/discourse/discourse-bbcode/pull/31

--- spec/pretty_text_spec.rb
+++ spec/pretty_text_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 describe PrettyText do
   it 'can apply color bbcode' do
     cooked = PrettyText.cook "hello [color=red]RED[/color] world"
-    html = '<p>hello <font color="red">RED</font> world</p>'
+    html = '<p>hello <span style="color:red">RED</span> world</p>'
 
     expect(cooked).to eq(html)
   end
