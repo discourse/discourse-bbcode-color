11a734b9a8d1a571d01feb667d617e52482004dc
David Taylor <david@taylorhq.com>
2022-12-29 12:30:23 +0000

    DEV: Introduce syntax_tree for ruby formatting (#43)

--- spec/pretty_text_spec.rb
+++ spec/pretty_text_spec.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 describe PrettyText do
-  it 'can apply color bbcode' do
+  it "can apply color bbcode" do
     cooked = PrettyText.cook "hello [color=red]RED[/color] world"
     html = '<p>hello <span style="color:red">RED</span> world</p>'
 
     expect(cooked).to eq(html)
   end
 
-  it 'can apply background color bbcode' do
+  it "can apply background color bbcode" do
     cooked = PrettyText.cook "hello [bgcolor=red]RED[/bgcolor] world"
     html = '<p>hello <span style="background-color:red">RED</span> world</p>'
 
