3336efe231e6c02e1dbe11ada71296af258cff58
Jarek Radosz <jradosz@gmail.com>
2024-01-15 12:24:11 +0100

    DEV: Update linting (#53)

--- plugin.rb
+++ plugin.rb
@@ -6,7 +6,6 @@
 # version: 0.2
 # authors: Neil Lalonde, Régis Hanol
 # url: https://github.com/discourse/discourse-bbcode-color
-# transpile_js: true
 
 after_initialize do
   chat&.enable_markdown_feature("bbcode-color") if respond_to?(:chat) && SiteSetting.chat_enabled
